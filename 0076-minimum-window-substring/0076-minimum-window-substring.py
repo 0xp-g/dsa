ac3477f57432f04b83e42c34b10ca410b6547986
Yogesh O K <iamnotyogesh@gmail.com>
2025-09-23 18:17:16 +0530

    Time: 86 ms (45.65%), Space: 18.2 MB (54.13%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,26 +1,19 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not t or not s:
-            return ""
-        needs_map = Counter(t)
-        required = len(needs_map)
-        window_counts = {}
-        formed = 0
-        left = 0
-        min_len = float('inf')
-        ans_indices = (-1, -1)
-        for right, char in enumerate(s):
-            window_counts[char] = window_counts.get(char, 0) + 1
-            if char in needs_map and window_counts[char] == needs_map[char]:
+        needsmap = Counter(t);windowmap = dict()
+        reqlen = len(needsmap);formed = 0
+        left = 0;ans_indices = [float('-inf'), float('-inf')]
+        minlen = float('inf')
+        for right in range(len(s)):
+            windowmap[s[right]] = windowmap.get(s[right], 0) + 1
+            if s[right] in needsmap and needsmap[s[right]] == windowmap[s[right]]:
                 formed += 1
-            while left <= right and formed == required:
-                current_len = right - left + 1
-                if current_len < min_len:
-                    min_len = current_len
-                    ans_indices = (left, right)
-                left_char = s[left]
-                window_counts[left_char] -= 1
-                if left_char in needs_map and window_counts[left_char] < needs_map[left_char]:
+            while left <= right and formed == reqlen:
+                if right - left + 1 < minlen:
+                    minlen = min(minlen, right - left + 1)
+                    ans_indices = [left, right]
+                windowmap[s[left]] = windowmap.get(s[left], 0) - 1
+                if s[left] in needsmap and windowmap[s[left]] < needsmap[s[left]]:
                     formed -= 1
                 left += 1
-        return "" if min_len == float('inf') else s[ans_indices[0] : ans_indices[1] + 1]
+        return "" if minlen == float('inf') else s[ans_indices[0] : ans_indices[1] + 1]
