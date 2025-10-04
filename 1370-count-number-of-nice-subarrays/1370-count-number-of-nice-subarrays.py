5cbf0dfc956c2b03117436f6efcb1e5b09882b10
Yogesh O K <iamnotyogesh@gmail.com>
2025-10-04 20:54:07 +0530

    Time: 175 ms (5.01%), Space: 23.1 MB (43.28%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
@@ -1,16 +1,16 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        return self.atMost(nums, k) - self.atMost(nums, k-1)
-    def atMost(self, nums, k):
-        hmap = {'odd' : 0}
-        res = 0
-        l = 0
-        for r in range(len(nums)):
-            if nums[r] % 2 == 1:
-                hmap['odd'] += 1
-            while hmap['odd'] > k:
-                if nums[l] % 2 == 1:
-                    hmap['odd'] -= 1
-                l += 1
-            res += r - l + 1
-        return res        
+        def atMost(nums, k):
+            hmap = {'odd' : 0}
+            res = 0
+            l = 0
+            for r in range(len(nums)):
+                if nums[r] % 2 == 1:
+                    hmap['odd'] += 1
+                while hmap['odd'] > k:
+                    if nums[l] % 2 == 1:
+                        hmap['odd'] -= 1
+                    l += 1
+                res += r - l + 1
+            return res   
+        return atMost(nums, k) - atMost(nums, k-1)     
