2186f5edef06c81846bc08221f98c5644fc22240
Yogesh O K <iamnotyogesh@gmail.com>
2026-03-06 11:49:58 +0530

    Time: 321 ms (97.11%), Space: 21.2 MB (88.52%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -3,17 +3,17 @@ class Solution:
         graph = defaultdict(list)
         for u, v, w in times:
             graph[u].append((v, w))
-        heap = [(k, 0,)]
+        heap = [(0, k)]
         dist = [inf] * (n+1)
         dist[k] = 0
         while heap:
-            node, current_dist = heappop(heap)
+            current_dist, node = heappop(heap)
             if current_dist > dist[node]:
                 continue
             for neighbor, nei_dist in graph[node]:
                 if current_dist + nei_dist < dist[neighbor]:
                     dist[neighbor] = current_dist + nei_dist
-                    heappush(heap, (neighbor, dist[neighbor]))
+                    heappush(heap, (dist[neighbor], neighbor))
         maxtime = 0
         for time in dist[1:]:
             if time == inf:
