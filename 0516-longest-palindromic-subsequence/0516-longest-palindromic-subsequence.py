7a4c2fc381b777d768b5beb02d668937a89faa49
Yogesh O K <iamnotyogesh@gmail.com>
2026-03-20 18:58:07 +0530

    Time: 1067 ms (32.31%), Space: 44.3 MB (46.71%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -5,11 +5,8 @@ class Solution:
         dp = [[0] * (n+1) for _ in range(n+1)]
         for i in range(n-1, -1, -1):
             for j in range(n-1, -1, -1):
-                if i == n or j == n:
-                    continue
+                if s1[i] == s2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
                 else:
-                    if s1[i] == s2[j]:
-                        dp[i][j] = 1 + dp[i+1][j+1]
-                    else:
-                        dp[i][j] = max(dp[i][j+1], dp[i+1][j])
+                    dp[i][j] = max(dp[i][j+1], dp[i+1][j])
         return dp[0][0]
