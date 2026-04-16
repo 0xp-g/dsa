ae657a4348cd7ade1ff2608e858dfd3fa4c845a3
Yogesh O K <iamnotyogesh@gmail.com>
2026-04-16 20:08:23 +0530

    Time: 396 ms (5.01%), Space: 43 MB (5.15%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -0,0 +1,49 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        hmap = dict()
+        cnt = 1
+        for num in nums:
+            if num in hmap:
+                continue
+            else:
+                hmap[num] = cnt
+                cnt += 1
+        
+        dsu = DSU(len(nums) + 1, hmap)
+
+        for i in range(len(nums)):
+            if nums[i] + 1 in hmap:
+                dsu.union(hmap[nums[i]], hmap[nums[i] + 1])
+            if nums[i] - 1 in hmap:
+                dsu.union(hmap[nums[i]], hmap[nums[i] - 1])
+        
+        return max(dsu.size)
+        
+class DSU:
+
+    def __init__(self, n, node_map):
+        self.par = [i for i in range(n+1)]
+        self.size = [1] * (n+1)
+    
+    
+    def find(self, u):
+        if self.par[u] != u:
+            par_u = self.find(self.par[self.par[u]])
+            self.par[u] = par_u
+        return self.par[u]
+    
+    def union(self, u, v):
+        par_u = self.find(u)
+        par_v = self.find(v)
+        if par_u != par_v:
+            if self.size[par_u] > self.size[par_v]:
+                self.size[par_u] += self.size[par_v]
+                self.par[par_v] = par_u
+            else:
+                self.size[par_v] += self.size[par_u]
+                self.par[par_u] = par_v
+            return True
+        else:
+            return False              
