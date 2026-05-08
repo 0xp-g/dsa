adf373267edf71e9c1287409978a050c261aa2dd
0xp-g <iamnotyogesh@gmail.com>
2026-05-08 21:10:09 +0530

    Time: 103 ms (40.01%), Space: 23.6 MB (97.91%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,29 +1,18 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-
-        MOD = 10**9 + 7 #3 pass solution. this solution uses pse and nse to calculate how much influence does an element has as a minimum value. left area * right area * value itself.
-        res = 0
-        n = len(arr)
-        pse = [-1] * n
-        nse = [n] * n
-        st1 = []
-        st2 = []
-
-        for i, v in enumerate(arr):
-            while st1 and v <= arr[st1[-1]]:
-                st1.pop()
-            if st1:
-                pse[i] = st1[-1]
-            st1.append(i)
-
+        n, MOD = len(arr), 10 ** 9 + 7
+        pse, nsee, st, res = [-1] * n, [n] * n, [], 0
         for i in range(n-1, -1, -1):
-            while st2 and arr[i] < arr[st2[-1]]:
-                st2.pop()
-            if st2:
-                nse[i] = st2[-1]
-            st2.append(i)
-
+            while st and arr[st[-1]] > arr[i]:
+                st.pop()
+            if st: nsee[i] = st[-1]
+            st.append(i)
+        st = []
         for i in range(n):
-            res += (i - pse[i]) * (nse[i] - i) * arr[i]
-        
+            while st and arr[st[-1]] >= arr[i]:
+                st.pop()
+            if st: pse[i] = st[-1]
+            st.append(i)
+        for i in range(n):
+            res += arr[i] * (i - pse[i]) * (nsee[i] - i) #(y-x) * (z-y) subarrays
         return res % MOD
