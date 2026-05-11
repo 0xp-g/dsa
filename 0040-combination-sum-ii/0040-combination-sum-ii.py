862ee69d49bbd35ed9d2af89ec251575d8683989
0xp-g <iamnotyogesh@gmail.com>
2026-05-11 20:47:30 +0530

    Time: 35 ms (14.75%), Space: 19.5 MB (42.04%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -8,10 +8,10 @@ class Solution:
                 return
             if i >= n or csum > target:
                 return
-            cel.append(nums[i])
+            cel.append(nums[i]) #nums[i] picked here
             backtrack(i+1, cel, csum+nums[i])
             cel.pop()
-            for idx in range(i, n):
+            for idx in range(i, n): #im setting up to ignore all the nums[i] so that in the next depth i pick something that is not equal to nums[i] (bcos if we pick nums[i] in the next depth, it is guaranteed that we will have a duplicate combination as we already picked nums[i] above.)
                 if nums[idx] != nums[i]:
                     backtrack(idx, cel, csum)
                     break
