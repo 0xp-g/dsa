5e9ff781b5022c493a594ffdf759ec8e0f04206d
0xp-g <iamnotyogesh@gmail.com>
2026-05-12 11:11:50 +0530

    Time: 0 ms (100%), Space: 19.3 MB (61.8%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,18 +1,19 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         n = len(nums)
-        low = 0
-        mid = 0
+        low = mid = 0
         high = n-1
-        while high >= mid:
+        while low <= mid and mid <= high:
             if nums[mid] == 0:
                 nums[low], nums[mid] = nums[mid], nums[low]
                 low += 1
                 mid += 1
             elif nums[mid] == 2:
-                nums[mid], nums[high] = nums[high], nums[mid]
-                high -= 1
-                #don't increment mid yet. nums[mid] can be 0!
+                nums[high], nums[mid] = nums[mid], nums[high]
+                high -= 1 #dont increment mid yet as it can have '0' which was swapped from high, we need to make sure it goes to low by the next iteration
             else:
                 mid += 1
         return nums
