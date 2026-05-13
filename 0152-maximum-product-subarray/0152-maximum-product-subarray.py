dbb74810d087f009ebfb2f660fb649d57c95ad5d
0xp-g <iamnotyogesh@gmail.com>
2026-05-13 20:28:37 +0530

    Time: 2 ms (93.71%), Space: 20 MB (20.94%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,16 +1,11 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        maxsofar = nums[0]
-        minsofar = nums[0]
-        globalmax = nums[0]
-        if len(nums) == 1:
-            return nums[-1]
-        for i, v in enumerate(nums):
-            if i == 0:
-                continue
-            temp = minsofar
-            tm = maxsofar
-            minsofar = min(nums[i], nums[i]*temp, nums[i]*tm)
-            maxsofar = max(nums[i], nums[i]*temp, nums[i]*tm) #max can become smaller when exploring deeper/ thus a global var that always stores the max state is required
-            globalmax = max(globalmax, maxsofar)
-        return globalmax
+        lmin = nums[0]
+        lmax = nums[0]
+        gmax = nums[0]
+        for x in nums[1:]:
+            temp = lmax
+            lmax = max(lmax * x, x, lmin*x)
+            lmin = min(temp*x, x, lmin*x)
+            gmax = max(gmax, lmax)
+        return gmax
