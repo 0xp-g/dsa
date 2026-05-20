1e0fac6b60c968ed19821a9aa4e44aec9bf45f19
0xp-g <iamnotyogesh@gmail.com>
2026-05-20 22:02:17 +0530

    Time: 21 ms (28.61%), Space: 19.8 MB (10.04%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,75 +1,35 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-
-        '''board = [['.'] * n] * n
         res = []
-        hset = set()
-        ls = []
-        def conflict(nr, nc, ls) -> bool:
-            nonlocal res
-            for r, c in ls:
-                if r == nr or c== nc or abs(r-nr) == abs(c-nc):
+
+        def violation_xists(i, j, rowmap, colmap, coords):
+            if i in rowmap:
+                return True
+            elif j in colmap:
+                return True
+            for r, c in coords: 
+                if abs(r-i) == abs(c-j):
                     return True
             return False
+        
+        def backtrack(r, rowmap, colmap, coords):
+            if r == n:
+                board = [['.'] * n for _ in range(n)]
+                for i, j in coords:
+                    board[i][j] = 'Q'
+                for i in range(n):
+                    board[i] = ''.join(board[i])
+                res.append(board[:])
+                return True
 
-        def solve(r, c, ls) :
-            if r == n-1:
-                res.append(ls[:])
-                return
-
-            for nr in range(r+1, n):
-                for nc in range(0, n):
-                    if conflict(nr, nc, ls):
-                        if nc == n-1:
-                            return False
-                        continue
-                    else:
-                        ls.append([nr, nc])
-                        #if not solve(nr, nc, ls): 
-                        #    return
-                        ls.pop()
-        solve(-1, -1, [])
-        for x in res:
-            print(x)'''
-
-        res = []
-        hset = set()
-        ls = []
-        def conflict(nr, nc, ls) -> bool:
-            nonlocal res
-            for r, c in ls:
-                if r == nr or c== nc or abs(r-nr) == abs(c-nc):
-                    return True
-            return False
-
-        def solve(r, c, ls):
-            if r == n-1 and len(ls) == n:
-                res.append(ls[:])
-                return
-
-            for nr in range(r+1, n):
-                for nc in range(0, n):
-                    if conflict(nr, nc, ls):
-                        if nc == n-1:
-                            return False
-                        continue
-                    else:
-                        ls.append([nr, nc])
-                        solve(nr, nc, ls)
-                        ls.pop()            
-        solve(-1, -1, [])
-
-        for x in res:
-            print(x)
-
-        ans = []
-        for valid in res:
-            board = [['.'] * n for _ in range(n)]
-            for x, y in valid:
-                board[x][y] = "Q"
-            temp = []
-            for row in board:
-                s= ''.join(row)
-                temp.append(s)
-            ans.append(temp)
-        return ans
+            for i in range(n):
+                if not violation_xists(r, i, rowmap, colmap, coords):
+                    rowmap.add(r)
+                    colmap.add(i)
+                    coords.append((r, i))
+                    backtrack(r+1, rowmap, colmap, coords)
+                    rowmap.discard(r)
+                    colmap.discard(i)
+                    coords.pop()
+        backtrack(0, set(), set(), [])
+        return res
