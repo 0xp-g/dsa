a9ed6a8854926d450db4a46c22024a12a31e3a4b
0xp-g <iamnotyogesh@gmail.com>
2026-06-01 18:12:16 +0530

    Time: 7 ms (67.82%), Space: 20.9 MB (36.88%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,17 +1,39 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        dq = deque()
-        dq.append((src, 0, 0))
-        dist = [float('inf')] * n
         graph = defaultdict(list)
+
         for u, v, w in flights:
             graph[u].append((v, w))
-        while dq:
-            node, dsf, depth = dq.popleft()
-            if depth > k:
+
+        INF = float('inf')
+
+        # dist[node][flights_used]
+        dist = [[INF] * (k + 2) for _ in range(n)]
+        dist[src][0] = 0
+
+        pq = [(0, src, 0)]  # cost, node, flights_used
+
+        while pq:
+            cost, node, flights_used = heapq.heappop(pq)
+
+            if node == dst:
+                return cost
+
+            if cost > dist[node][flights_used]:
                 continue
-            for nei, wei in graph[node]:
-                if dist[nei] > dsf + wei and depth <= k:
-                    dist[nei] = dsf + wei
-                    dq.append((nei, dist[nei], depth + 1))
-        return dist[dst] if dist[dst] != float('inf') else -1
+
+            if flights_used == k + 1:
+                continue
+
+            for nei, price in graph[node]:
+                new_cost = cost + price
+                new_flights = flights_used + 1
+
+                if new_cost < dist[nei][new_flights]:
+                    dist[nei][new_flights] = new_cost
+                    heapq.heappush(
+                        pq,
+                        (new_cost, nei, new_flights)
+                    )
+
+        return -1
