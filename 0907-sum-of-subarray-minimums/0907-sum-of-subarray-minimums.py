782cf6b4a0908a62cbce965ee5e092419c9673fd
0xp-g <iamnotyogesh@gmail.com>
2026-06-03 20:38:28 +0530

    Time: 105 ms (34.37%), Space: 23.8 MB (88.02%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,18 +1,22 @@
 class Solution:
-    def sumSubarrayMins(self, arr: List[int]) -> int:
-        n, MOD = len(arr), 10 ** 9 + 7
-        pse, nsee, st, res = [-1] * n, [n] * n, [], 0
+    def sumSubarrayMins(self, nums: List[int]) -> int:
+        MOD = (10 ** 9) + 7
+        n = len(nums)
+        st = []
+        res = 0
+        nse = [n] * n
+        psee = [-1] * n
         for i in range(n-1, -1, -1):
-            while st and arr[st[-1]] > arr[i]:
+            while st and nums[st[-1]] >= nums[i]:
                 st.pop()
-            if st: nsee[i] = st[-1]
+            if st:nse[i] = st[-1]
             st.append(i)
         st = []
         for i in range(n):
-            while st and arr[st[-1]] >= arr[i]:
+            while st and nums[st[-1]] > nums[i]:
                 st.pop()
-            if st: pse[i] = st[-1]
+            if st:psee[i] = st[-1]
             st.append(i)
         for i in range(n):
-            res += arr[i] * (i - pse[i]) * (nsee[i] - i) #(y-x) * (z-y) subarrays
+            res += ((nse[i] - i) * (i - psee[i]) * nums[i]) % MOD
         return res % MOD
