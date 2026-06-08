90f3c51c4b0cc065a2ad73fd09395b753cfba3c0
0xp-g <iamnotyogesh@gmail.com>
2026-06-08 17:28:33 +0530

    Time: 1457 ms (5.01%), Space: 613.2 MB (5.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,15 +1,22 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
-        @lru_cache(None)
-        def dp(i, target):
-            if target == 0:
+        @cache
+        def dp(i, rem):
+            if rem == 0:
                 return 0
-            if i >= n or target < 0:
-                return float('inf')
-            take = 1 + dp(i, target - coins[i])
-            nottake = 0 + dp(i + 1, target)
-            res = min(take, nottake)
-            return res
-        res =  dp(0, amount)
-        return res if res != float('inf') else -1
+            
+            if i >= n:
+                return inf
+            
+            if rem < 0:
+                return inf
+            
+            take = 1 + dp(i, rem-coins[i])
+            not_take = dp(i+1, rem)
+
+            return min(take, not_take)
+        
+        res = dp(0, amount)
+        print(res)
+        return res if res != inf else -1
